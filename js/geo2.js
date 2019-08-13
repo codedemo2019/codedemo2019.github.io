dfee1a946a2f3ccf071ce79d176e8e74c75b38f0
developerecotek <developer.ecotek@gmail.com>
2019-08-13 15:45:20 +0700

    page 1

---
+++ js/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "TRAN",
+      'continent': "AS",
+      'country': "VN",
+      'region': ""
+    },
+    'ip':"171.241.52.228"
+  }]);
+})
+//
+()
+
+;
---
+++ js/nv-loaded.js
@@ -0,0 +1,1 @@
+(function(){document.cookie=`_aa7988=1xbaea;path=/;domain=.digikey.com${window.location.protocol === "https:" ? "; secure" : ""}`;})()
